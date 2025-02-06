485d89d5816b0bacea396cb6405a837cfcd54780
maida209 <maida.a.ismail@gmail.com>
2025-02-06 07:08:09 -0600

    Adding to the corresponding Dealer

--- ICS-372-Assignment-1/src/main/java/org/example/Main.java
+++ ICS-372-Assignment-1/src/main/java/org/example/Main.java
@@ -8,23 +8,33 @@ import org.json.simple.parser.ParseException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
+        Scanner s = new Scanner(System.in);
         Set<Dealer> dealerSet = new HashSet<Dealer>();
 
-        String path = "src/main/java/org/example/inventory.json";
+        // asking user for file path
+        System.out.println("Enter JSON file path: ");
+        String filePath = s.next();
 
+        JSONParse(filePath, dealerSet);
+
+
+    }
+
+    static void JSONParse(String path, Set<Dealer> dealerSet) throws IOException {
         JSONParser parse = new JSONParser();
-        try{
+        try {
             FileReader read = new FileReader(path);
             JSONObject mainJsonObject = (JSONObject) parse.parse(read);
 
             JSONArray carInventory = (JSONArray) mainJsonObject.get("car_inventory");
 
-            for(Object vehicleObj : carInventory){
+            for (Object vehicleObj : carInventory) {
                 JSONObject vehicle = (JSONObject) vehicleObj;
 
                 String type = (String) vehicle.get("vehicle_type");
@@ -35,20 +45,7 @@ public class Main {
                 Long price = (Long) vehicle.get("price");
                 Long acquisitionDate = (Long) vehicle.get("acquisition_date");
 
-
-
-                System.out.println("Vehicle Type: " + type);
-                System.out.println("Manufacturer: " + manufacturer);
-                System.out.println("Model: " + model);
-                System.out.println("Price: $" + price);
-                System.out.println("Dealer ID: "+ dealershipId);
-                System.out.println("ID: " + id);
-                System.out.println("Acquisition Date: " + acquisitionDate);
-                System.out.println("----------------------");
-
-
-
-               Vehicle newVehicle;
+                Vehicle newVehicle;
                 switch (type.toLowerCase()) {
                     case "suv":
                         newVehicle = new SUV(id, manufacturer, model, acquisitionDate, price);
@@ -67,15 +64,41 @@ public class Main {
                         continue;
                 }
 
+                // checking if dealer already has an instance
+                boolean found = false;
+                for (Dealer d : dealerSet) {
+                    if (d.getDealerID().equals(dealershipId)) {
+                        d.enableAcquisition();
+                        d.addVehicle(newVehicle);
+                        found = true;
+                        break;
+                    }
 
+                }
+                // if dealer does not have instance, create a new instance
+                if (!found) {
+                    Dealer d = new Dealer(dealershipId);
+                    d.enableAcquisition();
+                    d.addVehicle(newVehicle);
+                    dealerSet.add(d);
+                }
 
             }
 
-        }catch (ParseException e){
+
+        } catch (ParseException e) {
             System.out.println("Not Opening");
         }
 
+        // printing  each dealer's vehicles
+        for (Dealer d : dealerSet) {
+            System.out.println("-----------------");
+            System.out.println("Dealer " + d.getDealerID());
+            System.out.println("-----------------");
+            for (Vehicle v : d.getVehicles()) {
+                System.out.println("Vehicle ID: " + v.getVehicleID());
 
-
+            }
+        }
     }
 }
