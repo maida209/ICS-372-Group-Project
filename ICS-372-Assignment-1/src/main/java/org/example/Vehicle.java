848dfe38644004a29924198662badeb80a9def3f
Mysha <myhsam98@gmail.com>
2025-02-06 00:09:15 -0600

    Add Comments in Vehicle class

--- ICS-372-Assignment-1/src/main/java/org/example/Vehicle.java
+++ ICS-372-Assignment-1/src/main/java/org/example/Vehicle.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import java.time.LocalDate;
-
 //Create abstract vehicle class
 abstract public class Vehicle {
     protected String vehicleID;
@@ -11,47 +9,54 @@ abstract public class Vehicle {
     protected double price;
     protected String type;
 
-    public Vehicle(String vehicleID, String manufacturer, String model, long acquisitionDate, double price, String type) {
-        this.vehicleID = vehicleID;
-        this.manufacturer = manufacturer;
-        this.model = model;
-        this.acquisitionDate = acquisitionDate;
-        this.price = price;
-        this.type = type;
-    }
-
-    public String getVehicleID() { return vehicleID; }
-    public String getManufacturer() { return manufacturer; }
-    public String getModel() { return model; }
-    public long getAcquisitionDate() { return acquisitionDate; }
-    public double getPrice() { return price; }
-    public String getType() { return type; }
+//Constructor
+public Vehicle(String vehicleID, String manufacturer, String model, long acquisitionDate, double price, String type) {
+    this.vehicleID = vehicleID;
+    this.manufacturer = manufacturer;
+    this.model = model;
+    this.acquisitionDate = acquisitionDate;
+    this.price = price;
+    this.type = type;
 }
 
+// Getter methods to retrieve the values of instance variables/attributes
+public String getVehicleID() { return vehicleID; }
+public String getManufacturer() { return manufacturer; }
+public String getModel() { return model; }
+public long getAcquisitionDate() { return acquisitionDate; }
+public double getPrice() { return price; }
+public String getType() { return type; }
+}
 
-
-
+//Create SUV subclass, which extends the Vehicle class
 class SUV extends Vehicle {
-    public SUV(String vehicleID, String manufacturer, String model, long acquisitionDate, double price) {
-        super(vehicleID, manufacturer, model, acquisitionDate, price, "suv");
+//Constructor
+public SUV(String vehicleID, String manufacturer, String model, long acquisitionDate, double price) {
+    super(vehicleID, manufacturer, model, acquisitionDate, price, "suv");
     }
 }
 
+//Create Sedan subclass, which extends the Vehicle class
 class Sedan extends Vehicle {
-    public Sedan(String vehicleID, String manufacturer, String model,long  acquisitionDate, double price) {
-        super(vehicleID, manufacturer, model, acquisitionDate, price, "sedan");
+//Constructor
+public Sedan(String vehicleID, String manufacturer, String model,long  acquisitionDate, double price) {
+    super(vehicleID, manufacturer, model, acquisitionDate, price, "sedan");
     }
 }
 
+//Create Pickup subclass, which extends the Vehicle class
 class Pickup extends Vehicle {
-    public Pickup(String vehicleID, String manufacturer, String model, long acquisitionDate, double price) {
-        super(vehicleID, manufacturer, model, acquisitionDate, price, "pickup");
+//Constructor
+public Pickup(String vehicleID, String manufacturer, String model, long acquisitionDate, double price) {
+    super(vehicleID, manufacturer, model, acquisitionDate, price, "pickup");
     }
 }
 
+//Create SportsCar subclass, which extends the Vehicle class
 class SportsCar extends Vehicle {
-    public SportsCar(String vehicleID, String manufacturer, String model, long acquisitionDate, double price) {
-        super(vehicleID, manufacturer, model, acquisitionDate, price, "sports car");
+//Constructor
+public SportsCar(String vehicleID, String manufacturer, String model, long acquisitionDate, double price) {
+    super(vehicleID, manufacturer, model, acquisitionDate, price, "sports car");
     }
 }
 
